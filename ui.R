8a53e067bd970e4e51c156c2068638405e080be4
Shlok Asrani <shloky1@uw.edu>
2019-03-12 19:54:15 -0700

    added by shlok
    Re-done UI page

--- ui.R
+++ ui.R
@@ -13,36 +13,35 @@ library(httr)
 library(spotifyr)
 library(ggjoy)
 library(shiny)
-
+library(rvest)
+library(shinyWidgets)
 
 
 Sys.setenv(SPOTIFY_CLIENT_ID = "dff090c9a456431a98b5eef00145e487")
 Sys.setenv(SPOTIFY_CLIENT_SECRET = "924d43f14b574ee2bf2b6fbce9d021f9")
 access_token <- get_spotify_access_token()
 
-country <- read.csv("data_averages/japan_data.csv", stringsAsFactors = F)
-
-
-
 ui <- navbarPage(
-  "Spotify INFO 201",
+  "Feelings, Emotions and Music",
   tabPanel(
     img(
-      " About", 
-      src = "logo.jpg", 
-      width = "40px", 
-      height = "40px"
+      "", 
+      src = "spotify.ico", 
+      width = "30px", 
+      height = "30px"
       ),
     sidebarLayout(
-      img(src = "logo.jpg", width = "600px", height = "600px"),
+      img(src = "logo.png", width = "250px", height = "250px", align = 'right-center'),
       mainPanel(
-        h1("About"),
+        h1("Spotify and it's API"),
+        
         p("Spotify is a major music streaming platform available on almost all
           devices. With hundreds of millions of users, Spotify collects a lot
           of data on users and their listening habits. With the Spotify API,
           Spotify shares much of its findings with the community. Many insights
-          can be made with the data provided through the API. Much of the
-          findings of the API are can be cleaned up and be useful to a 
+          can be made with the data provided through the API."),
+        
+        p("Much of the findings of the API are can be cleaned up to be user-friendly and relevant to a 
           wide range of people, including but not limited to free users, 
           subscription-paying members, Spotify employees, shareholders, 
           artists, distributors, music industrial analysts, music industry 
@@ -50,51 +49,60 @@ ui <- navbarPage(
           anyone involved with the music creation process. The following data
           is made possible with the Spotify API, the Spotifyr package, and R."
           ),
-        h2("The valence of albums"),
-        p("Using the Spotify API, this site plots the average valence of an
-          artist on a joyplot. This allows users to see an artists \"happiest\"
-          or \"saddest\" album."
-          ),
-        h2("Placeholder"),
-        p("Placeholder"),
         
-        h2("Questions that can be answered"),
-        p("Using the Spotify API, executives and producers can focus their
+        h3("What Is Valence?"),
+        
+        p("You might remember the  “valence” from high school chemistry. It has to do with how many electrons an atom will lose, gain, or share when it joins with another atom.
+Psychologists put a spin on that concept, using the word “valence” to describe whether something is likely to make someone feel happy (positive valence) or sad (negative valence).
+It’s sort of like the electron sense of the word. Is an event, situation, or experience going to add to your mood, or detract from it? That’s how you can calculate its emotional valence.
+(Spotify Insights, 2013)"),
+        
+        h3("But How Does A Numerical Value Measure Human Emotion?"),
+        
+        p("An individuals' reaction to music is emotional. Some of it makes us happy, and some of it makes us sad, with songs falling all across the spectrum between happy and sad.
+ It’s no easy feat to have a computer listen to a song in three seconds and determine its emotional valence, but Spotify figured out a way to do exactly that.  A music expert classifies sample songs by valence, then uses machine-learning to extend those rules to all of the rest of the music in the world, fine tuning as time passes. This makes the values more accurate as time passes."),
+        
+        h3("Questions that can be answered"),
+        p("Using our platform and analytics, executives and producers can focus their
           marketing efforts on what has proven to be successful in the past.
           Likewise, people can see all the successful songs/albums and attempt
-          to replicate that success.        
-          In a more detailed example, recognizing the valence of an album can 
-          be useful to an artist in many ways. One possible insight that is 
+          to replicate that success."),        
+         p("In a more detailed example, recognizing the valence of an album can 
+          be useful to an artists in many ways. One possible insight that is 
           relevant to an artist is which of their albums sell more, albums 
           with a high valence (happier albums) or albums with a low valence 
-          (sadder albums). With this knowledge, in the future an artist can 
-          create music that has a higher change of selling well."
-          )
+          (sadder albums). 
+With this knowledge, An artist can now create music that has a higher change of success and mass popularity; something crucial to the identity of music in popular culture. Valence data is perfect data to refer to
+           for new music development; This app will help artists make music that is more likely to be popular by providing never-seen-before musical insights such as
+           past and current trends, comparisons, and country-by-country data. Some potential questions include: "
+          ),
+        h5("What mood should my music invoke?"),
+        h5("I'm going on concert to the United Kingdom and Japan. Which tracks off my total music library should I play in each country?"),
+        h5("Why is my music so popular in the clubs not for individual listening?")
         
       )
     )
   ),
   tabPanel(
     img(
-      " Valence of an artists albums", 
-      src = "smile.png", 
-      width = "40px", 
-      height = "40px"
+      " Valence Data"
       ),
     sidebarLayout(
       sidebarPanel(
-        h1("Choose an artist (should work for most major artists)"),
+        h1("Choose An Artist"),
         textInput(
           "artistname",
-          "Enter the name of an artist",
+          "Enter The Name Of An Artist",
           "Beatles"
         ),
         p(
-          "The average 'valence' of all the songs in an album.
-          Valence is defined by Spotify as the musical positiveness conveyed
+          "The average 'valence' of all the songs in an album."),
+        p("Valence is defined by Spotify as the musical positiveness conveyed
           by a track. More happy, cheerful, and upbeat songs have a higher
           valence while songs that have more sad, angry, and depressed tones
-          have a lower valence. All the songs in an album have a valence value
+          have a lower valence." ),
+       
+ p("All the songs in an album have a valence value
           and the valence of the album is calculated by the mean of all the 
           songs in the albums valence added together and divided by the
           number of songs. It is presented on a 0.0 to 1.0 scale, with 0.0
@@ -104,24 +112,17 @@ ui <- navbarPage(
         )
       ),
       mainPanel(
-        plotOutput("chart")
+        (plotOutput("chart"))
       )
     )
   ),
   tabPanel(
     "Visualisation Two",
     sidebarLayout(
-      sidebarPanel(
-        selectInput(
-          "optionname",
-          "Select an option to see",
-          colnames(country),
-          "avg_valence"
-        )
-      ),
-      mainPanel(
-        plotlyOutput("chart2")
-      )
+      sidebarPanel(),
+      mainPanel()
     )
   )
 )
+
+
